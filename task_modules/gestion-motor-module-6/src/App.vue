4d6d8754206d5032da79a54ec8e2152d7c6e554f
KrBaIO3 <krbaio3@gmail.com>
2017-10-19 00:10:41 +0200

    feat(task)
    gm5 vue-cli webpack
    gm6 vue-cli webpack-simple, lista de tareas component

---
+++ task_modules/gestion-motor-module-6/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="container">
+    <div class="jumbotron">
+      <titulo></titulo>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: 'Lista de Tareas'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+
+}
+
+</style>
