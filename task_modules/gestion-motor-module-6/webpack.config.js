5d8f3f5b96f241c39708077689a71d863853b440
KrBaIO3 <krbaio3@gmail.com>
2017-10-20 18:22:50 +0200

    chore(webpack, index, package.json)
    webpack: *.css, icones y organizado de carpeta
    index: favicon
    package.json: bootstrap & font-awesome

--- task_modules/gestion-motor-module-6/webpack.config.js
+++ task_modules/gestion-motor-module-6/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
+            'css': 'vue-style-loader!css-loader',
             'scss': 'vue-style-loader!css-loader!sass-loader',
             'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
@@ -29,11 +30,18 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/
       },
+      { 
+        test: /\.(woff|woff2|eot|ttf)/,
+        loader: 'file-loader',
+        options: {
+          name: './fonts/[name].[ext]?[hash]'
+        }
+      },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
+          name: './img/[name].[ext]?[hash]'
         }
       }
     ]
