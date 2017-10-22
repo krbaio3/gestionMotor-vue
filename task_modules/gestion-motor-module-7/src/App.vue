621abbbec71a543d08cd6f3fa9276aa962625b76
KrBaIO3 <krbaio3@gmail.com>
2017-10-22 13:10:11 +0200

    feat(Slots)
    Primer paso Slots

---
+++ task_modules/gestion-motor-module-7/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <mostrarFormulario></mostrarFormulario>
+    </div>
+  </div>
+</template>
+<script>
+import MostrarFormulario from './scripts/mostrarFormulario.vue'
+export default {
+  components: {
+    mostrarFormulario : MostrarFormulario
+  },
+  data() {}
+};
+</script>
+
+<style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap';
+
+</style>
---
+++ task_modules/gestion-motor-module-7/src/scripts/mostrarFormulario.vue
@@ -0,0 +1,31 @@
+<template>
+    <form action="" class="form">
+        <h3>Iniciar Sesion</h3>
+            <div class="form-group">
+                <label for="correo">Correo Electronico</label>
+            <input type="email" 
+                    name="" 
+                    id="correo" 
+                    class="form-control"
+                    placeholder="Escribe tu Correo electronico">
+            </div>
+            <div class="form-group">
+                <label for="clave">Password</label>
+                <input type="password" 
+                        name="" 
+                        id="clave" 
+                        class="form-control" 
+                        placeholder="Escribe el Pass" 
+                        aria-describedby="helpId">
+                <small id="helpId" class="text-muted">Help text</small>
+            </div>
+        <button class="btn btn-primary">Iniciar Sesion</button>      
+  </form>
+</template>
+<script>
+    
+</script>
+<style lang="scss" scoped>
+
+
+</style>
