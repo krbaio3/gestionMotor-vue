b72e0f3daa8383202e920394921b2754e2d28126
KrBaIO3 <krbaio3@gmail.com>
2017-10-23 14:21:19 +0200

    feat(directivas)
    pasar argumentos a directiva

--- task_modules/gestion-motor-module-8/src/main.js
+++ task_modules/gestion-motor-module-8/src/main.js
@@ -4,6 +4,12 @@ import App from './App.vue'
 Vue.directive('decorar', {
     bind(el, binding, vnode) {
       el.style.fontFamily = binding.value;
+      if(binding.arg === 'grande'){
+        el.style.fontSize = '50px';
+      }
+      if (binding.arg === 'peque') {
+        el.style.fontSize = '10px';
+      }
       if(binding.modifiers['negrilla']){
         el.style.fontWeight = 'bold';
       } else if (binding.modifiers['italica']){
