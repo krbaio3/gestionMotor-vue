de48f657c2bf92863c3438500b675d60c320cafe
KrBaIO3 <krbaio3@gmail.com>
2017-10-25 20:22:44 +0200

    feat(filtro)
    Filtro local

--- task_modules/gestion-motor-module-9/src/App.vue
+++ task_modules/gestion-motor-module-9/src/App.vue
@@ -1,39 +1,27 @@
 <template>
   <div class="container">
     <div class="jumbotron">
-      <button class="btn btn-primary" 
-              @click="componenteSeleccionado = 'iniciarSesion'">IniciarSesion</button>
-      <button class="btn btn-primary" 
-              @click="componenteSeleccionado = 'cambiarClave'">Cambiar Clave</button>
-      <keep-alive>
-        <component :is="componenteSeleccionado"></component>
-      </keep-alive>
+      <span>{{ mensaje | mayusculas }}</span>
+      <hr>
+      <span>{{mensaje}}</span>
     </div>
   </div>
 </template>
 <script>
-import MostrarFormulario from './scripts/mostrar-formulario.vue';
-import IniciarSesion from './scripts/init-session.vue';
-import CambiarClave from './scripts/change-password.vue'; 
-
 export default {
-  components: {
-    mostrarFormulario : MostrarFormulario,
-    iniciarSesion : IniciarSesion,
-    cambiarClave : CambiarClave,
-  },
   data() {
     return {
-      componenteSeleccionado: 'iniciarSesion'
+      mensaje: 'Aprende VueJS 2 facilmente'
+    };
+  },
+  filters: {
+    mayusculas (msg) {
+      return msg.toUpperCase();
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-@import '../node_modules/bootstrap/scss/bootstrap';
-
-h3 {
-  color: blue;
-}
+@import "../node_modules/bootstrap/scss/bootstrap";
 </style>
