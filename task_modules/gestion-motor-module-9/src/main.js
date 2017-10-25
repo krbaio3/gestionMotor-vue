c603255d7d46a4f2751b5e6d3a7952570a993962
KrBaIO3 <krbaio3@gmail.com>
2017-10-25 21:26:54 +0200

    feat(http) Use vueResource

--- task_modules/gestion-motor-module-9/src/main.js
+++ task_modules/gestion-motor-module-9/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueResource from 'vue-resource';
 
 Vue.filter('suspensivos', function(texto) {
     return texto.substring(0, 10) + '...';
 });
+
+Vue.use(VueResource);
 
 new Vue({
     el: '#app',
