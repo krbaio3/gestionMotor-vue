3dec12c5e23794e9e33f6dcd96f4d46347bdd74e
KrBaIO3 <krbaio3@gmail.com>
2017-11-05 22:25:23 +0100

    feat(router)
    refactor router

--- task_modules/gestion-motor-module-11/src/components/encabezado.component.vue
+++ task_modules/gestion-motor-module-11/src/components/encabezado.component.vue
@@ -3,10 +3,10 @@
     <nav class="navbar navbar-light bg-faded">
       <h3><strong>Aplicacion Usuarios</strong></h3>
         <ul class="nav nav-pills pull-right">
-          <b-link router-tag="li" class="nav-item nav-link" to="/" exact>
-            <a href="">Inicio</a>
+          <b-link router-tag="li" class="nav-link" to="/" active-class="active" exact>
+            <a class="nav-item" href="">Inicio</a>
           </b-link>
-          <b-link router-tag="li" class="nav-item nav-link" to="/usuarios">
+          <b-link router-tag="li" class="nav-item nav-link" to="/usuarios" active-class="active">
             <a href="">Usuarios</a>
           </b-link>
       </ul>
