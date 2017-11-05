10d6012b8b480e67ac0d89388aade5289d8e778a
KrBaIO3 <krbaio3@gmail.com>
2017-11-05 21:53:49 +0100

    feat(router)
    router funcionando con BootstrapVue

--- task_modules/gestion-motor-module-11/src/components/encabezado.component.vue
+++ task_modules/gestion-motor-module-11/src/components/encabezado.component.vue
@@ -2,13 +2,13 @@
   <div class="header clearfix">
     <nav class="navbar navbar-light bg-faded">
       <h3><strong>Aplicacion Usuarios</strong></h3>
-        <ul class="nav">
-        <li class="nav-item">
-          <router-link class="nav-link" to='/'>Inicio</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to  ='/usuarios'>Usuarios</router-link>
-        </li>
+        <ul class="nav nav-pills pull-right">
+          <b-link router-tag="li" class="nav-item nav-link" to="/" exact>
+            <a href="">Inicio</a>
+          </b-link>
+          <b-link router-tag="li" class="nav-item nav-link" to="/usuarios">
+            <a href="">Usuarios</a>
+          </b-link>
       </ul>
     </nav>
   </div>
