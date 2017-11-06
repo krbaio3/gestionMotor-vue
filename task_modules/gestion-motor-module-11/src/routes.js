cd53e5479f031e648e1f0e4e4d6cf5c06ca463c0
KrBaIO3 <krbaio3@gmail.com>
2017-11-06 20:53:47 +0100

    feat(LazyLoading)
    Carga vaga

--- task_modules/gestion-motor-module-11/src/routes.js
+++ task_modules/gestion-motor-module-11/src/routes.js
@@ -1,6 +1,13 @@
 import InicioComponent from './components/inicio.component.vue';
-import UsuariosComponent from './components/usuarios.component.vue';
+// import UsuariosComponent from './components/usuarios.component.vue';
 import InfoUsuarioComponent from './components/infoUsuario.component.vue';
+
+// Esto es para carga vaga
+const UsuariosComponent = resolve => {
+  require.ensure(['./components/usuarios.component.vue'], () => {
+    resolve(require('./components/usuarios.component.vue'));
+  });
+}
 
 export const rutas = [
   {path:'', component:InicioComponent},
