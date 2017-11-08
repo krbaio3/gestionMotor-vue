d1bb879a081c0e5d01c10ea7e915779475ce274d
KrBaIO3 <krbaio3@gmail.com>
2017-11-08 17:05:45 +0100

    feat(holaMundo)
    primera prueba

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,9 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/holaMundo.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <barra-navegacion></barra-navegacion>
+        <div class="jumbotron">
+            <h1>Hola: {{saludo}}</h1>
+      </div>
+</div>
+</template>
+<script>
+import barraNavegacion from './barraNavegacion';
+
+export default {
+  components: { 'barra-navegacion': barraNavegacion },
+  data() {
+    return {
+      saludo: 'Jorge',
+    };
+  },
+};
+</script>
+<style lang="postcss" scoped>
+
+</style>
