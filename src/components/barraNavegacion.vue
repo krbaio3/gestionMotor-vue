889aa76a5a92177c6fe859ede28983e6372f489c
KrBaIO3 <krbaio3@gmail.com>
2018-01-21 21:51:02 +0100

    feat(Home)
    cabeceras

--- src/components/barraNavegacion.vue
+++ src/components/barraNavegacion.vue
@@ -4,7 +4,7 @@
 
         <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-        <b-navbar-brand href="#" disabled>Inicio</b-navbar-brand>
+        <b-navbar-brand href="#" disabled>Home</b-navbar-brand>
         <b-collapse is-nav id="nav_collapse">
 
           <b-navbar-nav>
@@ -20,21 +20,21 @@
               <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
             </b-nav-form>
 
-            <b-nav-item-dropdown text="Lang" right>
-              <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-              <b-dropdown-item href="#">RU</b-dropdown-item>
-              <b-dropdown-item href="#">FA</b-dropdown-item>
+            <b-nav-item href="#" class="fa fa-cog">Settings</b-nav-item>
+
+            <b-nav-item-dropdown text="User" class="fa fa-user" right>
+              <b-dropdown-item href="#">Mi Cuenta</b-dropdown-item>
+              <b-dropdown-item href="#">Acerca de...</b-dropdown-item>
+              <b-dropdown-item href="#">Desconexion</b-dropdown-item>
             </b-nav-item-dropdown>
 
-            <b-nav-item-dropdown right>
-              <!-- Using button-content slot -->
+            <!-- <b-nav-item-dropdown right>
               <template slot="button-content">
                 <em>User</em>
               </template>
               <b-dropdown-item href="#">Profile</b-dropdown-item>
               <b-dropdown-item href="#">Signout</b-dropdown-item>
-            </b-nav-item-dropdown>
+            </b-nav-item-dropdown> -->
           </b-navbar-nav>
 
         </b-collapse>
---
+++ src/components/footerNav.vue
@@ -0,0 +1,32 @@
+<template>
+   <div id="footer">
+      
+        <a href="">
+          <i class="fa fa-twitter"></i>
+        </a>
+      
+        <a href="">
+          <i class="fa fa-google-plus"></i>
+        </a>
+      
+        <a href="">
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+        </a>
+  </div>
+</template>
+<script>
+export default {
+  name: 'FooterNav',
+};
+</script>
+<style lang="scss" scoped>
+#footer {
+  background-color: red;
+  li {
+    list-style-type: none;
+  }
+  a {
+    color: white;
+  }
+}
+</style>
