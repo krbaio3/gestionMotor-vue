dfe8cd01efc01fd050950f628c101ad764fc6d0e
KrBaIO3 <krbaio3@gmail.com>
2018-02-23 21:00:17 +0100

    feat(clean)

--- task_modules/gestion-motor-module-home/src/components/footer-component.tsx
+++
@@ -1,9 +0,0 @@
-import Vue, { VNode } from 'vue';
-
-export const FooterComponent = Vue.extend({
-    render(h):  VNode{
-        return (
-            <h1>Footer Component</h1>
-        );
-    },
-});
